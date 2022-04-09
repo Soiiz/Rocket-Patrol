eb4fe000f9a36c1a31f391193407abf22a6ebd5e
Soiiz <kchau6@ucsc.edu>
2022-04-09 01:02:51 -0700

    Saving

--- src/main.js
+++ src/main.js
@@ -6,9 +6,8 @@ let config = {
 }
 let game  = new Phaser.Game(config);
 
-// reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
-
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
